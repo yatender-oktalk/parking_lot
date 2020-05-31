cc82c987997ed40d9ae2e146f9a3a92a96ba3025
Yatender Singh <yatender.nitk@gmail.com>
2020-05-31 06:36:24 +0530

    Removed the scafolded method
    - Scafolded method `hello` has been removed from the `ParkingLot` Module

--- lib/parking_lot.ex
+++ lib/parking_lot.ex
@@ -2,17 +2,4 @@ defmodule ParkingLot do
   @moduledoc """
   Documentation for `ParkingLot`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ParkingLot.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
