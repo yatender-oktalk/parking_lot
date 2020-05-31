55239aa147f3ccfe6989b4813e4fb6a722e04b05
Yatender Singh <yatender.nitk@gmail.com>
2020-05-31 07:25:20 +0530

    Added parking lot struct
    - Parking lot module `ParkingLot.Core.ParkingLot` added.
    parking lot is having 3 properties
    right now `id`, `slots, and `used_slots`
    For future purposes this has been assumed
    that we can extend this to multiple parking slots.

---
+++ lib/core/parking_lot.ex
@@ -0,0 +1,9 @@
+defmodule ParkingLot.Core.ParkingLot do
+  @moduledoc """
+  A module which takes care of the parking lot
+  """
+
+  defstruct id: 1,
+            slots: [],
+            used_slots: []
+end
