805d4175e5c0d2b165c0162e526c8b883f26cd03
Yatender Singh <yatender.nitk@gmail.com>
2020-05-31 08:22:17 +0530

    Updated module `ParkingLot.Core.ParkingLot`
    - Added `new/1` which generates a new parking lot with the given slots.

--- lib/core/parking_lot.ex
+++ lib/core/parking_lot.ex
@@ -3,7 +3,28 @@ defmodule ParkingLot.Core.ParkingLot do
   A module which takes care of the parking lot
   """
 
-  defstruct id: 1,
+  alias ParkingLot.Core.{Slot}
+
+  defstruct id: nil,
             slots: [],
             used_slots: []
+
+  @spec new(integer()) :: %{:__struct__ => atom, optional(atom) => any}
+  def new(slot_count) do
+    parking_struct = struct!(__MODULE__, id: System.unique_integer([:monotonic, :positive]))
+
+    parking_struct
+    |> add_slots(slot_count)
+  end
+
+  # Private functions
+
+  defp add_slots(%__MODULE__{} = parking_lot, slot_count) do
+    slots =
+      1..slot_count
+      |> Stream.map(&Slot.new(id: &1))
+      |> Enum.to_list()
+
+    %{parking_lot | slots: slots}
+  end
 end
