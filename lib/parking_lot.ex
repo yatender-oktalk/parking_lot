0aeb16c8c041e943b7305e90fbbf42b78b8dbdc8
Yatender Singh <yatender.nitk@gmail.com>
2020-05-31 06:20:07 +0530

    Project setup
    - Setup project by running command `mix new parking_lot`.

---
+++ lib/parking_lot.ex
@@ -0,0 +1,18 @@
+defmodule ParkingLot do
+  @moduledoc """
+  Documentation for `ParkingLot`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ParkingLot.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/parking_lot_test.exs
@@ -0,0 +1,8 @@
+defmodule ParkingLotTest do
+  use ExUnit.Case
+  doctest ParkingLot
+
+  test "greets the world" do
+    assert ParkingLot.hello() == :world
+  end
+end
