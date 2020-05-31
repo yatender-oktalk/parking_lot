56e3e1c44a42cc477e3e0a0c28a9ee9964db1477
Yatender Singh <yatender.nitk@gmail.com>
2020-05-31 08:23:13 +0530

    Updated the module `ParkingLot.Core.Slot` - Added `specs` for the `new/1`

--- lib/core/slot.ex
+++ lib/core/slot.ex
@@ -6,6 +6,7 @@ defmodule ParkingLot.Core.Slot do
   @enforce_keys ~w(id)a
   defstruct ~w(id ticket_id type)a
 
+  @spec new(Keyword.t() | map()) :: %{:__struct__ => atom, optional(atom) => any}
   def new(fields) do
     struct!(__MODULE__, fields)
   end
